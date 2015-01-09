876da59fdffd4b6498bd07b9707b9e6dc41d033e
Min RK <benjaminrk@gmail.com>
2015-01-09 15:06:26 -0800

    remove user-id from userlist
    no longer used in upstream

--- roles/jupyterhub/files/make_userlist.sh
+++ roles/jupyterhub/files/make_userlist.sh
@@ -8,11 +8,11 @@ users=$(cut -d: -f1 /etc/passwd)
 for user in $users; do
     is_instructor=$(groups $user | grep "\\binstructors\\b")
     if [[ ! -z "$is_instructor" ]]; then
-        echo "$user:$(id -u $user) admin" >> $1
+        echo "$user admin" >> $1
     fi
 
     is_student=$(groups $user | grep "\\bstudents\\b")
     if [[ ! -z "$is_student" ]]; then
-        echo "$user:$(id -u $user)" >> $1
+        echo "$user" >> $1
     fi
 done
