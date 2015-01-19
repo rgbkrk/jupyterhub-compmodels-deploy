1bddc15eefb81a3e5c3e66a964748d4ea3105d56
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-01-18 16:57:33 -0800

    Only install monitoring agent if it is not present

--- roles/common/files/monitoring.sh
+++ roles/common/files/monitoring.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -e
 
-sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-14.04-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-curl https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc | apt-key add -
-apt-get update
-apt-get install rackspace-monitoring-agent
-rackspace-monitoring-agent --setup --username $1 --apikey $2
-service rackspace-monitoring-agent start
+if [ -z $(which rackspace-monitoring-agent) ]; then
+    sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-14.04-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
+    curl https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc | apt-key add -
+    apt-get update
+    apt-get install rackspace-monitoring-agent
+    rackspace-monitoring-agent --setup --username $1 --apikey $2
+    service rackspace-monitoring-agent start
+else
+    service rackspace-monitoring-agent restart
+fi
