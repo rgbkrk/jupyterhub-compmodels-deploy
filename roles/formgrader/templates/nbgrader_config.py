222c0cc84d758254238a0e25dd4e48c60c8caaf6
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-07-05 10:52:51 -0700

    Turn hardcoded things into variables

--- roles/formgrader/templates/nbgrader_config.py
+++ roles/formgrader/templates/nbgrader_config.py
@@ -1,10 +1,20 @@
 c = get_config()
 
-c.NbGraderConfig.course_id = 'cogsci131'
+c.NbGraderConfig.course_id = '{{ course_id }}'
+
 c.FormgradeApp.ip = "127.0.0.1"
 c.FormgradeApp.port = 9000
 c.FormgradeApp.authenticator_class = "nbgrader.auth.hubauth.HubAuth"
-c.HubAuth.graders = ["jhamrick"]
-c.HubAuth.notebook_url_prefix = "class_files"
+
+c.HubAuth.notebook_url_prefix = "{{ nbgrader_root }}"
 c.HubAuth.hubapi_address = "{{ servicenet_ip }}"
-c.HubAuth.hub_base_url = "https://compmodels.tmpnb.org"
+c.HubAuth.hub_base_url = "{{ hub_base_url }}"
+
+# Add users to the grader list
+graders = set(["{{ nbgrader_user }}"])
+with open('/etc/ipython/graderlist') as f:
+    for line in f:
+        if line.isspace():
+            continue
+        graders.add(line.strip())
+c.HubAuth.graders = list(graders)
